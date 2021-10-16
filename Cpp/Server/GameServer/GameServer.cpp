8ff24d1b4a8f62bd6cea28eeedd5a09705bbb35a
ge1204h <ge1204h@gmail.com>
2021-10-16 18:54:35 +0900

    * Atomic * 멀티스레드 사용 시 스택영역을 제외한 영역을 공유(코드, 힙, 데이터 영역 공유)

--- Cpp/Server/GameServer/GameServer.cpp
+++ Cpp/Server/GameServer/GameServer.cpp
@@ -1,49 +1,41 @@
 ﻿#include "pch.h"
-
 #include <thread>
+#include <atomic>
 
-void HelloThread()
+// atomic : All-Or-Nothing
+atomic<int32> sum = 0;
+void Add()
 {
-	cout << "Hello Thread" << endl;
+	for (int32 i = 0; i < 100'0000; i++)
+	{
+		sum.fetch_add(1);
+		//sum++;
+		// atomic을 사용하지 않은 상황에서의 레지스터 의사 코드
+		//int32 eax = sum;
+		//eax = eax + 1;
+		//sum = eax;
+	}
 }
-
-void HelloThread_2( int32 num )
+void Sub()
 {
-	cout << num << endl;
+	for (int32 i = 0; i < 100'0000; i++)
+	{
+		sum.fetch_add(-1);
+		//sum--;
+		//int32 eax = sum;
+		//eax = eax - 1;
+		//sum = eax;
+	}
 }
-
 int main()
 {
-	vector<thread> vec;
+	Add();
+	Sub();
+	cout << sum << endl;
 
-	for (int32 i = 0; i < 10; i++)
-	{
-		vec.push_back(thread(HelloThread_2, i));
-	}
-
-	for (int32 i = 0; i < 10; i++)
-	{
-		if (vec[i].joinable())
-			vec[i].join();
-	}
-
-	//thread te(HelloThread);
-	//thread t;
-	//auto id = t.get_id(); // id = 0
-
-	//t = thread(HelloThread);
-	//auto id = t.get_id(); // id 배정
-
-
-	//int32 count = t.hardware_concurrency(); // CPU 코어 개수, 정보를 추출 할 수 없다면 0을 출력할 때도 있음
-	//auto id = t.get_id(); // 쓰레드마다 id, 쓰레드 사이에는 안 겹친다는 법칙
-
-	//t.join(); // 쓰레드가 끝날때까지 main에서 기다려줌
-	//t.detach(); // std::thread 객체에서 실제 쓰레드를 분리, 독립적으로 쓰레드가 동작 할 수 있도록 해줌. 이 경우 추출해둔 쓰레드 정보를 알 수 없음 (보통 사용 x)
-	//t.joinable(); // 실질적으로 객체가 관리하고 있는 쓰레드가 살아 있는지 없는지
-
-	//if (t.joinable())
-	//	t.join();
-
-	cout << "Hello Main" << endl;
+	thread t1(Add);
+	thread t2(Sub);
+	t1.join();
+	t2.join();
+	cout << sum << endl;
 }
